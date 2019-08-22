5a51948deda4bcbb58e46ed81170096d5876bb99
zhanglongfeng <zhanglongfeng@oraro.net>
2019-08-22 17:52:55 +0800

    新建了一个简单的棋盘准备开始一个组件一个组件的堆彻
    Change-Id: I3df02bf96273a101859a550a7fbd1d262cba2e94

---
+++ src/chessboard/index.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './index.css'
+const chessboard : React.FC = () => {
+    return ( 
+            <div className="chessBoard"> 
+              <header>棋盘</header>
+            </div>
+            )
+}
+export default chessboard
